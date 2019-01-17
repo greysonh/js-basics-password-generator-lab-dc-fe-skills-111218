6e135e1f87de03b52b6c2dbf98e1054c6cdba3c8
Greyson Harris <gharris@esri.com>
2019-01-17 00:04:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-window.addEventListener("load", () => {
+window.addEventListener("load", function (e) {
   //start your code here
-});
+ 
+  var passwordLength
+  
+  document.getElementById("passwordSubmit").addEventListener("click", function (e) {
+    event.preventDefault();
+    passwordLength = document.getElementById("passwordLength").value;
+    
+    // calculatePassword(passwordLength);
+    document.getElementById("passwordResult").innerHTML = calculatePassword(passwordLength);
+  });
+
+  //This calculates a random password
+  function calculatePassword(length) {
+    var password = getUppercaseLetter() + getLowercaseLetter() + getSpecialChar() + getNumber();
+    
+    console.log(password);
+    return password;
+  }
+
+  function getUppercaseLetter() {
+    return String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  }
+
+  function getLowercaseLetter() {
+    return  getUppercaseLetter().toLowerCase();
+  }
+
+  function getSpecialChar() {
+    return String.fromCharCode(33 + Math.floor(Math.random() * 15));
+  }
+
+  function getNumber() {
+    return Math.floor(Math.random() * 10);
+  }
+  
+}); 
